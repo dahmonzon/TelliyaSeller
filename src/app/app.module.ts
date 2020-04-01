fe0863cf92a8589052f183c3677c1d4a4d69f991
money <money@dev.local>
2020-04-01 09:11:34 +0200

    add core,auth and menu component for setup purpose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { AuthComponent } from './auth/auth.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    AuthComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
