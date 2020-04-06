61f135b2e2d2dace9cdf6a9798929ddca0c6a357
monzon <diarradah402@gmail.com>
2020-04-06 20:37:05 +0200

    add app store configuration
    this configuration will allow feature module to add their own part of the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreComponent } from './core/core.component';
 import { AuthComponent } from './auth/auth.component';
 import { MenuComponent } from './menu/menu.component';
+import {reducers, metaReducers} from './appStore'
+
 
 @NgModule({
   declarations: [
@@ -16,6 +25,19 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictActionImmutability: true,
+        strictActionSerializability: true,
+        strictStateSerializability: true,
+        strictStateImmutability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    EffectsModule.forRoot([]),
     AppRoutingModule
   ],
   providers: [],
